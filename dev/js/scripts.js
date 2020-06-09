64bc293a2ced2706061f14ad94fd9af2e230e528
queeniechen79 <63672423+queeniechen79@users.noreply.github.com>
2020-06-09 19:39:01 -0400

    did stuff

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,34 @@
+// const track = document.querySelector('#food-carousel');
 
+$(document).ready(function() {
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidetoshow: 1,
+        slidestoscroll: 1,
+        mobilefirst: true,
+        adaptiveheight: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+            },
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidetoshow: 1,
+                    slidestoscroll: 1
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidetoshow: 1,
+                    slidestoscroll: 1
+                }
+            }
+        ]
+    });
+});
